3fc308627d85c47573661b99fb50a397dfbe4c2a
Jeremy Rodriguez <jeremy.rodrguez@yahoo.com>
2020-10-23 05:07:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  my_hash_creator
+  my_hash_creator = {
+    :name 'Grace Hopper'
+  }
 end
 
 def update_counting_hash(hash, key)
